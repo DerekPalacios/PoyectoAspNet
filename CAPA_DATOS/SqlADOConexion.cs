81010a6abe115e41794906d9fa486cb75a3e483c
Derek Palacios <derek2001corez@gmail.com>
2022-10-30 17:03:12 -0600

    prueba de linea desde master

--- CAPA_DATOS/SqlADOConexion.cs
+++ CAPA_DATOS/SqlADOConexion.cs
@@ -10,7 +10,7 @@ namespace CAPA_DATOS
         //static string SQLServer = "GEMELEK";
         static string SQLServer = "."; // para que supuestamente jale en cualquier server
 
-        //edicion de linea en prueba git
+        //edicion de linea en prueba git prueba de linea desde la master
         static public bool IniciarConexion(string user, string password)
         {
             try
