89c84cddf12e384393eeb40cfb071aebad951937
Derek Palacios <derek2001corez@gmail.com>
2022-10-30 16:45:53 -0600

    Cambio en la capa de datos de server por un punto para que cargue en el local

--- CAPA_DATOS/SqlADOConexion.cs
+++ CAPA_DATOS/SqlADOConexion.cs
@@ -7,7 +7,9 @@ namespace CAPA_DATOS
         private static string UserSQLConexion = "";
         public static SqlServerGDatos SQLM;
         public static string DataBaseName = "Poyecto";
-        static string SQLServer = "GEMELEK";
+        //static string SQLServer = "GEMELEK";
+        static string SQLServer = "."; // para que supuestamente jale en cualquier server
+
         static public bool IniciarConexion(string user, string password)
         {
             try
