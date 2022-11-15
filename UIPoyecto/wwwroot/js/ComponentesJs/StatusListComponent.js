96be5d4a5114c0c223cff8ef06e238a6a68179d6
MARIO CESAR VADO MARTÍNEZ <mario.vado1790@estu.unan.edu.ni>
2022-11-15 14:37:29 -0600

    Actualizacion 1, pero con mucho texto para que Derek la encuentre

---
+++ UIPoyecto/wwwroot/js/ComponentesJs/StatusListComponent.js
@@ -0,0 +1,25 @@
+﻿export default function CreateDataList(DataSet = [], UL, TableFunction, colums) {
+    UL.innerText = "";
+
+    DataSet.forEach((item, index) => {
+        const row = document.createElement("li");
+        for (var prop in item) {
+            const ld = document.createElement("div");
+            ld.innerText = item[prop];
+            row.append(ld);
+        }
+
+        const tdAction = document.createElement("div");
+        const btnTable = document.createElement("a");
+        btnTable.className = "link";
+        btnTable.href = "#";
+        btnTable.innerText = "Ver más...";
+        btnTable.onclick = () => {
+            TableFunction(item);
+        }
+        tdAction.append(btnTable);
+        row.append(tdAction);
+        row.classList.add('container', colums);
+        UL.append(row);
+    });
+}
---
+++ UIPoyecto/wwwroot/js/ComponentesJs/TaskListComponent.js
@@ -0,0 +1,14 @@
+﻿export default function CreateTaskList(DataSet = [], TaskList) {
+    TaskList.innerText = "";
+
+    DataSet.forEach((item, index) => {
+        const task = document.createElement('div');
+        for (var prop in item) {
+            task.innerText = item[prop];
+        }
+
+        task.classList.add('container', 'taskelement');
+        task.addEventListener('click', changeTaskState);
+        TaskList.prepend(task);
+    });
+}
