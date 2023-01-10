9373e256ece99b62a0f4a5d6f0374849910a4abb
will_08_PalCor <derek2001corez@gmail.com>
2023-01-10 01:11:02 -0600

    Modificacion de controller de alimento en la adicion de metodos de lectura de datos para alimento y lineas de alimentacion

--- UIPoyecto/Controllers/AlimentoController.cs
+++ UIPoyecto/Controllers/AlimentoController.cs
@@ -18,7 +18,7 @@ namespace UIPoyecto.Controllers
         }
 
         ///<summary>
-        ///Guarda galera basico
+        ///Guarda alimento basico
         ///</summary>
         ///<returns></returns>
 
@@ -28,6 +28,11 @@ namespace UIPoyecto.Controllers
             return NewAl.IdAlimento = (int)NewAl.Save();
         }
 
+        ///<summary>
+        ///Regresa informacion de alimento de simplificada
+        ///</summary>
+        ///<returns></returns>
+
         [HttpGet]
         public object GetAlimentos()
         {
@@ -36,13 +41,90 @@ namespace UIPoyecto.Controllers
                       {
                           IdAlimento = Alimento.IdAlimento,
                           Nombre = Alimento.Nombre,
-                          Descripcion = Alimento.Descripcion,
-                          MarcaId = Alimento.IdLineaAlimentoRecomendado
+                          Descripcion = Alimento.Descripcion
                       };
             return obj;
 
         }
 
+        ///<summary>
+        ///Regresa informacion de alimento completa
+        ///</summary>
+        ///<returns></returns>
+
+        [HttpGet]
+        public object GetAlimentoCompleto()
+        {
+            var obj = from Alimento in new Alimento().Get<Alimento>()
+                      select new
+                      {
+                          IdAlimento = Alimento.IdAlimento,
+                          Nombre = Alimento.Nombre,
+                          Descripcion = Alimento.Descripcion,
+                          Etapa = Alimento.cargarNombreEtapa(),
+                          Linea = Alimento.cargarNombreLinea(),
+                      };
+            return obj;
+
+        }
+
+        ///<summary>
+        ///Regresa lista basica de linea de alimentacion
+        ///</summary>
+        ///<returns></returns>
+
+        [HttpGet]
+        public object GetLineaAlimento()
+        {
+            var obj = from Linea in new LineaAlimento().Get<LineaAlimento>()
+                      select new
+                      {
+                          idLinea = Linea.IdLineaAlimento,
+                          Nombre = Linea.NombreLinea,
+                          Descripcion = Linea.DescripcionLinea,
+                          NombreMarca = Linea.cargarNombreMarcaAlimento()
+                      };
+            return obj;
+
+        }
+
+        ///<summary>
+        ///Regresa lista basica de Marca de linea de alimentacion
+        ///</summary>
+        ///<returns></returns>
+
+        [HttpGet]
+        public object GetMarcaLineaAlimento()
+        {
+            var obj = from Marca in new MarcaLineaAlimento().Get<MarcaLineaAlimento>()
+                      select new
+                      {
+                          IdMarca = Marca.IdMarca,
+                          Nombre = Marca.Nombre,
+                          Descripcion = Marca.DescripcionMarca
+                      };
+            return obj;
+
+        }
+
+        ///<summary>
+        ///Regresa lista basica de Marca de linea de alimentacion
+        ///</summary>
+        ///<returns></returns>
+
+        [HttpGet]
+        public object Get()
+        {
+            var obj = from Marca in new MarcaLineaAlimento().Get<MarcaLineaAlimento>()
+                      select new
+                      {
+                          IdMarca = Marca.IdMarca,
+                          Nombre = Marca.Nombre,
+                          Descripcion = Marca.DescripcionMarca
+                      };
+            return obj;
+
+        }
 
     }
 }
