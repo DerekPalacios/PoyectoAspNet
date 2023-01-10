83f0bfc85ceac60fa0fe381c136bfb33ea7a9e5d
will_08_PalCor <derek2001corez@gmail.com>
2023-01-10 01:10:03 -0600

    Adicion de funciones de extraccion de detalles de modelo de linea de alimento

--- CAPA_NEGOCIO/Models/LineaAlimento.cs
+++ CAPA_NEGOCIO/Models/LineaAlimento.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using CAPA_DATOS;
 
 #nullable disable
@@ -14,5 +15,18 @@ namespace CAPA_NEGOCIO.Models
         public int IdMarcaLinea { get; set; }
         public string DescripcionLinea { get; set; }
         public string NombreLinea { get; set; }
+        public string NombreMarcaLineaAlimento;
+
+        public LineaAlimento()
+        {
+
+        }
+
+        public string cargarNombreMarcaAlimento()
+        {
+            return new MarcaLineaAlimento().Get<MarcaLineaAlimento>("IdMarca = " + IdMarcaLinea)
+                .First().Nombre;
+        }
+
     }
 }
