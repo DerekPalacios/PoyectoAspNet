3d29a2fa6bb890477b466f0d1ff503835726baf9
will_08_PalCor <derek2001corez@gmail.com>
2023-01-11 09:10:18 -0600

    Adicion de metodo get con filtro como componente JS

---
+++ UIPoyecto/wwwroot/js/ComponentesJs/GetDatoByApiUrlFiltering.js
@@ -0,0 +1,15 @@
+﻿export default async function GetObjectDataByFilter(url, varName, varValue) {
+    const params = new URLSearchParams();
+    params.append(varName, varValue);
+    url = url + `?${params}`;
+    let response = await fetch(url, {
+        method: "GET",
+        headers: {
+            'Content-Type': "application/json; charset = utf-8",
+            'Accept': "*/*"
+        }
+    });
+
+    response = await response.json();
+    return response;
+}
