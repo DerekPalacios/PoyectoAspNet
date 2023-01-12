4a06820f8ef9a27d2f1e95e3d7e2b58153e0ea80
will_08_PalCor <derek2001corez@gmail.com>
2023-01-12 02:20:08 -0600

    adicion de vista de tratamienrtos basico para cargar informacion

---
+++ CAPA_NEGOCIO/Models/SubModel/VistaTratamientoBasico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CAPA_DATOS;
+using System.Linq;
+
+namespace CAPA_NEGOCIO.Models.SubModel
+{
+    public partial class VistaTratamientoBasico : EntityClass
+    {
+
+        public string IdTratamiento { get; set; }
+        public string Descripcion { get; set; }
+        public string Nombre { get; set; }
+        public string NombrePeriodicidad { get; set; }
+        public string TipoAdministracion { get; set; }
+        public string DosisDiariaRecomendada { get; set; }
+        public string DescripcionDosisdiaria { get; set; }
+        public string DosisTotalesRecomendadas { get; set; }
+        public string IdPeriodicidad { get; set; }
+        public string IdViaAdministracion { get; set; }
+
+
+
+
+
+
+    }
+}
