7fc3a38521f23d436d521f329fc5e74a59558c25
will_08_PalCor <derek2001corez@gmail.com>
2023-01-22 23:11:20 -0600

    implementacion de api de get cargo y correccion minima del modelo

--- CAPA_NEGOCIO/Models/TblCargoUsuario.cs
+++ CAPA_NEGOCIO/Models/TblCargoUsuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CAPA_DATOS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -7,17 +8,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CAPA_NEGOCIO.Models
 {
-    public partial class TblCargoUsuario
+    public partial class TblCargoUsuario : EntityClass
     {
-        public TblCargoUsuario()
-        {
-            TblUsuarios = new HashSet<TblUsuario>();
-        }
-
+       
         public int IdCargo { get; set; }
         public string DescripcionCargo { get; set; } = null!;
 
-        public virtual ICollection<TblUsuario> TblUsuarios { get; set; }
     }
 }
 
