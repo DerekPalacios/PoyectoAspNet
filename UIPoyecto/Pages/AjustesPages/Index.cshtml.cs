f3ae5c98b57f056a1929ad3cf101964b8bc89ed8
will_08_PalCor <derek2001corez@gmail.com>
2023-01-26 10:59:16 -0600

    Correcciones de Login

--- UIPoyecto/Pages/AjustesPages/Index.cshtml.cs
+++ UIPoyecto/Pages/AjustesPages/Index.cshtml.cs
@@ -1,16 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using CAPA_NEGOCIO.Security;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UIPoyecto.Pages.AjustesPages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            if (AuthNetCore.VerifyAuthenticate())
+            {
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("../Login");
+            }
+
         }
     }
 }
