87b01ad47e5db7798af6a9361f2a714bbf55d284
will_08_PalCor <derek2001corez@gmail.com>
2023-01-26 09:41:04 -0600

    Correcciones de login y adicion de metodos de login

--- UIPoyecto/Pages/Login.cshtml.cs
+++ UIPoyecto/Pages/Login.cshtml.cs
@@ -2,15 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CAPA_NEGOCIO.Security;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace UIPoyecto.Pages
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public string Usuario_Personal { get; set; }
+        [BindProperty]
+        public string Contrasena_Personal { get; set; }
+        public string Msg { get; set; }
+        public bool acceso { get; set; }
+        public IActionResult OnGet()
         {
+            acceso = AuthNetCore.VerifyAuthenticate();
+            if (acceso)
+            {
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (AuthNetCore.loginIN(Usuario_Personal, Contrasena_Personal))
+            {
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                Msg = "Datos incorrectos";
+                return Page();
+            }
         }
     }
 }
