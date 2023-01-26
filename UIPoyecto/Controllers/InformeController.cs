186c972fb0b55cc1b10d0d6697d7a3cb5a375fdc
will_08_PalCor <derek2001corez@gmail.com>
2023-01-26 14:57:40 -0600

    controller poara informes

---
+++ UIPoyecto/Controllers/InformeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CAPA_NEGOCIO.Models;
+using System.Collections.Generic;
+using CAPA_NEGOCIO.Security;
+using System.Linq;
+using System.Diagnostics;
+using static Humanizer.In;
+using System.Security.Cryptography;
+using CAPA_NEGOCIO.Models.SubModel;
+
+namespace UIPoyecto.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class InformeController : ControllerBase
+    {
+        public InformeController()
+        {
+
+            AuthNetCore.loginIN("admin", "admin");
+            //aca hay que insertar las validaciones de ussuarios 
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
