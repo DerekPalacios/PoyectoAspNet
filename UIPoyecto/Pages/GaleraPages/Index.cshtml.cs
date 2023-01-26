0bb82c629404bfffb354d6f1062cd2cf77e1b5d2
will_08_PalCor <derek2001corez@gmail.com>
2023-01-26 10:23:34 -0600

    CS models addeds
    Adicion de modelos cs para la manipulacion de login

---
+++ UIPoyecto/Pages/GaleraPages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CAPA_NEGOCIO.Security;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UIPoyecto.Pages.GaleraPages
+{
+    public class Index : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            if (AuthNetCore.VerifyAuthenticate())
+            {
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("../Login");
+            }
+
+        }
+    }
+}
---
+++ UIPoyecto/Pages/ProduccionPages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CAPA_NEGOCIO.Security;
+
+namespace UIPoyecto.Pages
+{
+    public class IndexProduccionModel : PageModel
+    {
+        private readonly ILogger<IndexProduccionModel> _logger;
+
+        public IndexProduccionModel(ILogger<IndexProduccionModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (AuthNetCore.VerifyAuthenticate())
+            {
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("../Login");
+            }
+
+        }
+    }
+}
