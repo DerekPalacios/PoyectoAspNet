ebd9fb99179655656ccf9f1a4b83287b16916092
will_08_PalCor <derek2001corez@gmail.com>
2023-01-26 09:41:29 -0600

    cambios genericos

--- UIPoyecto/Pages/Index.cshtml.cs
+++ UIPoyecto/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CAPA_NEGOCIO.Security;
 
 namespace UIPoyecto.Pages
 {
@@ -17,8 +18,16 @@ namespace UIPoyecto.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            if (AuthNetCore.VerifyAuthenticate())
+            {
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("Login");
+            }
 
         }
     }
